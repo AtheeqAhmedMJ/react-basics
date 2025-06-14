449cd10b985755450fcd3beabb0e8a3ecd14f18c
Mohamed Atheeq Ahmed M J <atheeqahmed@Mohameds-MacBook-Air.local>
2025-06-14 14:56:44 +0530

    Initial React project upload

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from './Footer';
+import Header from './Header';
+import Food from './Food';
+
+function App() {
+  return (
+    <>
+         <Header/>
+         <Food/>
+         <Footer/>
+    </>
+
+  )
+}
+
+export default App
+ 
---
+++ my-first-react-app/src/Food.jsx
@@ -0,0 +1,17 @@
+
+function Food(){
+    const food1 = "Italian";
+    const food2 = "Mexican";
+    const food3 = "Indian";
+    
+    return(
+        <ul>
+        <li> {food1}</li>
+        <li> {food2}</li>
+        <li> {food3}</li>
+        </ul>
+        
+
+    );
+}
+export default Food;
